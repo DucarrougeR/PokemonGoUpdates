1483ad48deeeeed122a92fc5b8f4eb901cf37faa
Romain <romainducarrouge@gmail.com>
2016-07-30 14:08:29 +0100

    commit

--- testing_Image.py
+++ testing_Image.py
@@ -12,27 +12,25 @@
 # Imports
 import time
 import datetime
-import webbrowser
 import os
 import shutil
 from PIL import ImageGrab
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+import smtplib
 
 # Variables
 URL = "https://dublin-town-south-pokemongo.herokuapp.com/"
 email = "my.name@address.com"
-web_tag = "fullmap"
-
+password = "your_password"
 d1 = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+
 
 
 def open_website(url):
 	# use selenium for Chrome
 	browser = webdriver.Chrome(executable_path=r"C:\Program Files (x86)\Google\Chrome\chromedriver.exe")
-	# maximize browser windos
+	# maximize browser window
 	browser.maximize_window()
 	browser.get(url)
 
@@ -52,7 +50,8 @@ def open_website(url):
 			src = "C:\\Users\\Romain\\Desktop\\PokemonGoUpdates\\" + name
 			dest = "C:\\Users\\Romain\\Desktop\\PokemonGoUpdates\\photos\\" + name
 			shutil.move(src, dest)	
-	
+
+		
 	
 def download_image():
 	''' function to download an image from url '''
@@ -60,8 +59,24 @@ def download_image():
 def move_image_to_folder():
 	''' move png file to 'photos' folder'''
 
-	
+def send_image_to_email():
+	''' use smtplib from gmail to gmail '''  
 
-open_website(URL)	
+	to = 'abc.def@gmail.com'
+	gmail_user = 'abc.def@gmail.com'
+	gmail_pwd = ''
+	smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+	smtpserver.ehlo()
+	smtpserver.starttls()
+	smtpserver.ehlo
+	smtpserver.login(gmail_user, gmail_pwd)
+	header = 'To:' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject:testing \n'
+	print (header)
+	msg = header + '\n this is test msg from mkyong.com \n\n'
+	smtpserver.sendmail(gmail_user, to, msg)
+	print ('done!')
+	smtpserver.close()
+
+#open_website(URL)	
 #download_image(URL)
-	
+send_image_to_email()
